50fed965736406845a9bfa8b4b7b2aa532fd5a78
alvadia <vadim1221@hotmail.com>
2019-10-07 17:37:50 +0300

    eliminate test

--- RVO.c
+++ RVO.c
@@ -1,14 +1,4 @@
 #include <iostream>
-#include <iostream>
-
-struct C {
-  C() = default;
-  C(const C&) { std::cout << "A copy was made.\n"; }
-};
-
-C F() {
-  return C();
-}
 
 class Printer {
     public:
